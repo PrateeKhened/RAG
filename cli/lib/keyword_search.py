734f02a89a7dd6902c8e3274e9ef896b9a1db9d9
prateek khened <code.prateekkhened@gmail.com>
2026-01-11 21:04:55 +0530

    remove puntuations and tokenize the query and the search title to search more efficiently

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,4 +1,5 @@
 from .search_utils import DEFAULT_SEARCH_LIMIT, load_movies
+import string
 
 def search(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     movies = load_movies()
@@ -6,14 +7,31 @@ def search(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     results = []
 
     for movie in movies:
-        preprocessed_query = preprocessed_text(query)
-        preprocessed_title = preprocessed_text(movie['title'])
-        if preprocessed_query in preprocessed_title:
+        preprocessed_query = tokenize_text(query)
+        preprocessed_title = tokenize_text(movie['title'])
+        if has_matching_token(preprocessed_query, preprocessed_title):
             results.append(movie)
             if len(results) > 5: 
                 break
-
     return results
 
+def has_matching_token(query_token: list[str], title_token: list[str]) -> bool:
+    for q in query_token:
+        for t in title_token:
+            if q in t:
+                return True
+    return False
+
+
 def preprocessed_text(text: str) -> str:
+    table = str.maketrans('', '', string.punctuation)
+    text = text.translate(table)
     return text.lower()
+
+def tokenize_text(text: str) -> list[str]:
+    text = preprocessed_text(text)
+    valid_tokens = []
+    for t in text.split():
+        if t:
+            valid_tokens.append(t)
+    return valid_tokens
